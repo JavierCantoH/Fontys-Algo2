2e152bc418ea23428e2ea94f05cc69176085076d
Luis Javier Canto Hurtado <javier.canto.h@gmail.com>
2022-11-08 23:03:07 +0100

    finish vertex cover

--- Assignments/Assignment1.py
+++ Assignments/Assignment1.py
@@ -1,3 +1,6 @@
+
+# TODO UI stuff: add comments to the code and button to restart textfields insted of closing and reopening every single time
+
 from tkinter.tix import InputOnly
 import PySimpleGUI as sg
 import matplotlib.pyplot as plt
@@ -27,7 +30,6 @@ class Graph(object):
                 elif getProbability(probability) == True:
                     randomNode = random.choice(range(self.vertices))
                     self.addSingleEdge(node, randomNode)
-    
 
 def printAdjMatrix(graph):
         A = np.array(graph.adjMatrix)
@@ -36,32 +38,46 @@ def printAdjMatrix(graph):
 def drawGraph(graph):
         A = np.array(graph.adjMatrix)
         G = nx.from_numpy_matrix(A)
-        nx.draw(G, node_color='lightblue')
+        nx.draw(G, node_color='lightblue', with_labels = 1)
+
+def bruteVertexCover(g):
+    cover = []
+    isValid, numOfEdgesInNode = validate(g, cover)
     
-def isNodeVertexCoverTrue(graph, start, num):
-        count = 0
-        visited = [False] * graph.vertices
-        DFS(graph, start, visited)
-        for nodeIndex, nodeValue in enumerate(visited):
-            if (nodeValue == True):
-                if nodeIndex == start:
-                    print("same node")
-                else:
-                    count = count + 1
-        if (num <= count):
-            return True
-        else:
-            return False
+    while not isValid:
+        #  nodeInVC = [x][0] the [0] because numOfEdgesInNode is a list of lists but with only 1 "[[0 0 0 0]]"
 
-def bruteVertexCover(graph, num):
-    for node in range(graph.vertices):
-        currentNode = isNodeVertexCoverTrue(graph, node, num)
-        if currentNode == True:
-            return ("Graph has at least one vertex cover of size: " + str(num))
-        else:
-            print("The current node doesn't have vertex cover")
-    return ("Graph doesn't have vertex cover of size: " + str(num))
+        #  example: (to reproduce use: 4 nodes, 50%)
+        #       1
+        #       |
+        #       0
+        #     /   \
+        #   2       3
+        #  in this graph the numOfEdgesInNode = [3, 1, 1, 1] (in the first iteration)
+        #  iterate from position 0...3 and we mark the node as cover if it has the max number of edges
+        nodeInVC = [x for x in range(0, len(numOfEdgesInNode)) if numOfEdgesInNode[x] == max(numOfEdgesInNode)][0]
+         #  in this case node 0, so we marked as covered
+        cover.append(nodeInVC)
+        # we validate the vertex cover
+        isValid, numOfEdgesInNode = validate(g, cover) 
+    # in the second iteration our vertex cover will be valid
+    return cover
 
+def validate(g, cover):
+    isValid = True
+    # we create a list of [[0, 0, ...]] (only 1 list inside the list size of the graph adjmatrix)
+    numOfEdgesInNode = [0] * len(g)
+    A = np.array(numOfEdgesInNode)
+    print(np.matrix(A))
+    # iterate through the adjMatrix
+    for i in range(0, len(g)):
+        for j in range(i, len(g)): # we start on i because we will be counting the edges twice otherwise
+            if g[i][j] == 1: # check if there is an edge between 2 nodes in position [i][j]
+                if (i not in cover) and (j not in cover): # in the second iteration, node 0 will be in cover, so we skip the next lines
+                    isValid = False
+                    numOfEdgesInNode[i] += 1  # (iteration 1) from 0 we can go to 1
+                    numOfEdgesInNode[j] += 1  # (iteration 1) from 1 we can go to 0
+    return isValid, numOfEdgesInNode
 
 def getProbability(probability):
         p = float(probability / 100)
@@ -94,7 +110,6 @@ def makeItConnected(graph):
             graph.addSingleEdge(node, random.choice(range(graph.vertices)))
             makeItConnected(graph)
 
-
 layout = [
         [sg.Button("EXIT")],
         [sg.Text("Enter the number of nodes:")],    
@@ -114,11 +129,6 @@ numberOfNodes: int
 probability: int
 g: Graph
 
-# TODO: button to restart textfields insted of closing and reopening every single time
-# TODO: add comments to the code
-
-# TODO: fix connected graph
-# TODO: complelty change brute vertex cover
 # TODO: week 3
 
 while True:
@@ -158,8 +168,12 @@ while True:
                 numOfVertexCover = int(input3)
                 print(f'Integer: {numOfVertexCover}')
                 printAdjMatrix(g)
-                text = bruteVertexCover(g, numOfVertexCover)
+                visited = [False] * (g.vertices)
+                text = ""
+                if len(bruteVertexCover(g.adjMatrix)) <= numOfVertexCover:
+                    text = "Graph has vertex cover of size: " + str(numOfVertexCover)
+                else:
+                    text = "Graph doesn't have vertex cover of size: " + str(numOfVertexCover)
                 window['-vertexCoverLabel-'].Update(text)
             except:
                 print("Not Integer")
-#window.close()
